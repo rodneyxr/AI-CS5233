ca6f7d46e7babae35b3fde4cd3ccb07797e1da92
Rodney Rodriguez <rodneyxr@gmail.com>
2016-09-30 03:29:31 -0500

    Finished ailab2

--- ailab2/src/dicegame/DiceGameSolver.java
+++ ailab2/src/dicegame/DiceGameSolver.java
@@ -1,17 +1,20 @@
 package dicegame;
-import java.io.*;
-import java.util.*;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class DiceGameSolver implements Runnable {
 	private Scanner in;
 	private PrintStream out;
-	public static final String[] DIE_VALUES = { "one", "two", "three", "four", "five", "six"};
+	public static final String[] DIE_VALUES = { "one", "two", "three", "four", "five", "six" };
 	private int numGames; // number of games to play
 
 	public DiceGameSolver(Scanner in, PrintStream out) {
 		this.in = in;
 		this.out = out;
-		numGames = 2;  // Change to 1000 for final version
+		numGames = 2; // TODO: Change to 1000 for final version
 	}
 
 	public void run() {
@@ -22,8 +25,8 @@ public class DiceGameSolver implements Runnable {
 		for (int game = 1; game <= numGames; game++) {
 			String line = null;
 			int evidence1 = 0, evidence2 = 0, evidence3 = 0;
-			List<String> guessList = new ArrayList<String>();  // remember previous guesses
-			List<Integer> evidenceList = new ArrayList<Integer>();  // remember evidence
+			List<String> guessList = new ArrayList<String>(); // remember previous guesses
+			List<Integer> evidenceList = new ArrayList<Integer>(); // remember evidence
 			do {
 				// Expecting thtree ints on this line.
 				line = in.nextLine();
@@ -52,12 +55,12 @@ public class DiceGameSolver implements Runnable {
 					return;
 				}
 				scan.close();
-				
+
 				// Add evidence to evidenceList
 				evidenceList.add(evidence1);
 				evidenceList.add(evidence2);
 				evidenceList.add(evidence3);
-				
+
 				// run Bayesian network to get next guess
 				int[] guesses = diceNetwork(evidenceList, guessList);
 				int guess1 = guesses[0];
@@ -71,73 +74,112 @@ public class DiceGameSolver implements Runnable {
 				line = in.nextLine();
 				if (line.contains("quit")) {
 					break;
-				} 
+				}
 			} while (line.contains("wrong"));
 		}
 
 		out.print("quit\n");
 	}
-	
+
 	// This returns the next guess.
 	// evidenceList is all the evidence bits that have been read
 	// guessList indicates previous guesses.
 	private int[] diceNetwork(List<Integer> evidenceList, List<String> guessList) {
 		BayesianNetwork bn = new BayesianNetwork();
-		
+
 		Variable die1 = new Variable("Die1", DIE_VALUES);
 		bn.addVariable(die1);
-        Factor dieFactor1 = new Factor(die1);
-        bn.addFactor(dieFactor1);
-        
-        dieFactor1.set(1.0 / 6.0, 0);
-        dieFactor1.set(1.0 / 6.0, 1);
-        dieFactor1.set(1.0 / 6.0, 2);
-        dieFactor1.set(1.0 / 6.0, 3);
-        dieFactor1.set(1.0 / 6.0, 4);
-        dieFactor1.set(1.0 / 6.0, 5);
-        
-        Variable die2 = new Variable("Die2", DIE_VALUES);
+		Factor dieFactor1 = new Factor(die1);
+		bn.addFactor(dieFactor1);
+
+		dieFactor1.set(1.0 / 6.0, 0);
+		dieFactor1.set(1.0 / 6.0, 1);
+		dieFactor1.set(1.0 / 6.0, 2);
+		dieFactor1.set(1.0 / 6.0, 3);
+		dieFactor1.set(1.0 / 6.0, 4);
+		dieFactor1.set(1.0 / 6.0, 5);
+
+		Variable die2 = new Variable("Die2", DIE_VALUES);
 		bn.addVariable(die2);
-        Factor dieFactor2 = new Factor(die2);
-        bn.addFactor(dieFactor2);
-        
-        dieFactor2.set(1.0 / 6.0, 0);
-        dieFactor2.set(1.0 / 6.0, 1);
-        dieFactor2.set(1.0 / 6.0, 2);
-        dieFactor2.set(1.0 / 6.0, 3);
-        dieFactor2.set(1.0 / 6.0, 4);
-        dieFactor2.set(1.0 / 6.0, 5);
-        
-        // You need to write the code to completely define the Bayesian network.
-        // For example, if evidenceList.size() is n, then you will need n
-        // evidence Variables and n Factors.
-        // You will also need to call bn.observe n times to set the values
-        // of the evidence variables.
-        //
-        // See BayesianNetworkTest.java for examples.
-        
-        Factor result1 = bn.eliminateVariables(die1);
-        Factor result2 = bn.eliminateVariables(die2);
-        
-        // result1.get(i-1) should be P(die1 = i | evidence)
-        int guess1 = -1; // need to add one later
-        int guess2 = -1; // need to add one later
-        for (int i = 1; i <= 6; i++) {
-        	for (int j = 1; j <= 6; j++) {
-        		if (! guessList.contains("" + i + j)) {
-        			if (guess1 == -1 ||
-        				result1.get(i-1) * result2.get(j-1) > 
-        				result1.get(guess1-1) * result2.get(guess2-1)) {
-        				guess1 = i;
-        				guess2 = j;
-        			}
-        		}
-        	}
-        }
-        	
-        int[] guesses = new int[2];
-        guesses[0] = guess1;
-        guesses[1] = guess2;
+		Factor dieFactor2 = new Factor(die2);
+		bn.addFactor(dieFactor2);
+
+		dieFactor2.set(1.0 / 6.0, 0);
+		dieFactor2.set(1.0 / 6.0, 1);
+		dieFactor2.set(1.0 / 6.0, 2);
+		dieFactor2.set(1.0 / 6.0, 3);
+		dieFactor2.set(1.0 / 6.0, 4);
+		dieFactor2.set(1.0 / 6.0, 5);
+
+		// You need to write the code to completely define the Bayesian network.
+		// For example, if evidenceList.size() is n, then you will need n
+		// evidence Variables and n Factors.
+		// You will also need to call bn.observe n times to set the values
+		// of the evidence variables.
+		//
+		// See BayesianNetworkTest.java for examples.
+		for (int i = 0; i < evidenceList.size(); i++) {
+			int e = evidenceList.get(i);
+			Variable v = new Variable("Evidence" + i + "" + (i % 3 + 1), DiceGame.EVIDENCE);
+			bn.addVariable(v);
+			Factor f = new Factor(die1, die2, v);
+			for (int d1 = 1; d1 <= 6; d1++) {
+				for (int d2 = 1; d2 <= 6; d2++) {
+					double p = 0.0;
+					switch (i % 3) {
+					case 0:
+						// probability evidence1 = 1 is (die1 + die2 - 2) / 10
+						p = (d1 + d2 - 2) / 10.0;
+						break;
+					case 1:
+						// probability evidence2 = 1 is 0 if die1 > die2, 1 if die1 < die2, else 1/2
+						if (d1 > d2)
+							p = 0;
+						else if (d1 < d2)
+							p = 1;
+						else
+							p = 0.5;
+						break;
+					case 2:
+						// probability evidence3 = 1 is 0 if both dice are even, 1 if both are odd, else 1/2
+						if (d1 % 2 == 0 && d2 % 2 == 0)
+							p = 0;
+						else if (d1 % 2 == 1 && d2 % 2 == 1)
+							p = 1;
+						else
+							p = 0.5;
+						break;
+					}
+					if (e == 0)
+						p = 1 - p;
+					f.set(p, d1 - 1, d2 - 1, e);
+				}
+			}
+			bn.addFactor(f);
+			bn.observe(v, DiceGame.EVIDENCE[e]);
+		}
+
+		Factor result1 = bn.eliminateVariables(die1);
+		Factor result2 = bn.eliminateVariables(die2);
+
+		// result1.get(i-1) should be P(die1 = i | evidence)
+		int guess1 = -1; // need to add one later
+		int guess2 = -1; // need to add one later
+		for (int i = 1; i <= 6; i++) {
+			for (int j = 1; j <= 6; j++) {
+				if (!guessList.contains("" + i + j)) {
+					if (guess1 == -1 || result1.get(i - 1) * result2.get(j - 1) > result1.get(guess1 - 1)
+							* result2.get(guess2 - 1)) {
+						guess1 = i;
+						guess2 = j;
+					}
+				}
+			}
+		}
+
+		int[] guesses = new int[2];
+		guesses[0] = guess1;
+		guesses[1] = guess2;
 		return guesses;
 	}
 }
