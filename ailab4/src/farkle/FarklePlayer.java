7fc3cb2fedfb93981564d3483bdf6a5bd568d388
Rodney Rodriguez <rodneyxr@gmail.com>
2016-11-18 17:34:45 -0600

    Finish ailab4

--- ailab4/src/farkle/FarklePlayer.java
+++ ailab4/src/farkle/FarklePlayer.java
@@ -4,6 +4,16 @@ import java.io.*;
 import java.util.*;
 
 public class FarklePlayer implements Runnable {
+	// Number of Dice 	Farkle Count 	Sum of Positive Rewards
+	// 1 				4 				150
+	// 2 				16 				1800
+	// 3 				60 				18750
+	// 4 				204 			183150
+	// 5 				600 			1675800
+	// 6 				1440 			14411250
+	final int[] positiveRewards = {0, 150, 1800, 18750, 183150, 1675800, 14411250};
+	final int[] farkleCount = {0, 4, 16, 60, 204, 600, 1440}; 
+	
     Scanner in; // for reading input
     PrintStream out; // for printing output
     int numTurns;  // number of Farkle turns to play
@@ -27,9 +37,11 @@ public class FarklePlayer implements Runnable {
         int turnScore = 0;
         while (! line.equals("quit")) {
         	Scanner readline = new Scanner(line);
+        	int[] count = new int[7];
         	for (int i = 0; i < 6; i++) {
         		if (readline.hasNextInt()) {
-        			dice[i] = readline.nextInt();     
+        			dice[i] = readline.nextInt();
+        			count[dice[i]]++;
         		} else {
         			readline.next();  // skip the X
         			dice[i] = -1;
@@ -40,18 +52,42 @@ public class FarklePlayer implements Runnable {
         	// Set aside 1s and 5s
         	// You will need to figure out how to set aside 3 of a kinds
         	String setAside = "";
+        	for (int i = 1; i <= 6; i++) {
+				if (count[i] >= 3) {
+					for (int d = 0, three = 0; d < 6; d++) {
+						if (dice[d] == i) {
+							setAside += (d + 1) + " ";
+							dice[d] = -1;
+							count[i] -= 1;
+							three++;
+						}
+						if (three == 3)
+							break;
+					}
+				}
+			}
+        	
         	for (int i = 0; i < 6; i++) {
         		if (dice[i] == 1 || dice[i] == 5) {
         			// dice are numbered from 1 to 6 so avoid off by one errors
         			int d = i + 1;
         			setAside += d + " ";
+        			dice[i] = -1;
         		}
         	}
         	out.print(setAside);
+        	int nDice = 6;
+        	for (int i = 0; i < 6; i++)
+        		if (dice[i] == -1)
+        			nDice--;
         	
         	// Now need to decide whether to bank or not.
         	// This program always decides to bank.
-        	boolean banked = true;
+        	// You should bank if the expected reward is less than your turn score.
+        	int pos = positiveRewards[nDice];
+        	int neg = -1 * farkleCount[nDice] * turnScore;
+        	int expectedReward = pos + neg;
+        	boolean banked = (expectedReward < turnScore);
         	
         	// Finish printing
         	if (banked) out.print("bank");
